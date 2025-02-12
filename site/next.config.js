dec8f407386bd9cea5e42ae6c5d11c8cbbbcfd99
Emrul Islam <emrul@emrul.com>
2025-02-12 13:08:45 +0000

    Update 1.1 branch

--- site/next.config.js
+++ site/next.config.js
@@ -2,10 +2,10 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Kasm AI Images',
+    description: 'The official store for Kasm AI workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://emrul.github.io/kasm_ai_registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
